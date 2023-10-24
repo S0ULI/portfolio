6781c344ec1cf6970f3b685c67a3311e891526f7
abolfazl <souli.fsd@gmail.com>
2023-10-25 01:41:13 +0330

    little fixes

--- app/components/projrcts/ProjectItem.jsx
+++ app/components/projrcts/ProjectItem.jsx
@@ -6,9 +6,17 @@ const ProjectItem = ({src, href, title, w, h}) => {
     <div className='flex flex-col gap-8'>
         <h3 className='hover:underline text-color-primary text-lg w-fit'><a href={href} target="_blank" rel={title}>{title}</a></h3>
         <div className='w-full flex justify-center items-center'>
-            <a href={href} target="_blank" rel={title} className='w-fit'>
+          {
+            href ? (
+              <a href={href} target="_blank" rel={title} className='w-fit'>
                 <Image src={src} alt={title} width={w} height={h} className='hover:-translate-y-2 transition-transform duration-300' />
-            </a>
+              </a>
+            ) : (
+              <div className='w-fit opacity-50'>
+                <Image src={src} alt={title} width={w} height={h} className='hover:-translate-y-2 transition-transform duration-300' />
+              </div>
+            )
+          }
         </div>
     </div>
   )
