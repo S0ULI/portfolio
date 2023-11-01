0314fc78200466c740ea1e9bf7b65eeb63629359
abolfazl <souli.fsd@gmail.com>
2023-11-01 21:09:57 +0330

    shop url updated

--- app/components/projrcts/Projects.jsx
+++ app/components/projrcts/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <div id="projects">
         <Wrapper myStyle='flex flex-col gap-8'>
             <h2 className="h2-style">Projrcts</h2>
-            <ProjectItem src='/image/project1.png' href='https://clone-flame-nine.vercel.app/' title='Clothing E-commerce' w={841} h={480} />
+            <ProjectItem src='/image/project1.png' href='https://shopper-temp.vercel.app/' title='Clothing E-commerce' w={841} h={480} />
             <ProjectItem src='/image/project2.png' title='Watchi' w={745} h={445} />
         </Wrapper>
     </div>
